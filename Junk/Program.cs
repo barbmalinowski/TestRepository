a8750e4b96d2656ee03261ac9a3b1cc05f975be2
Barb <bmalinowski@swtechnologies.com>
2014-09-30 10:42:43 -0400

    change

--- Junk/Program.cs
+++ Junk/Program.cs
@@ -16,6 +16,8 @@ namespace Junk
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // my change
         }
     }
 }
